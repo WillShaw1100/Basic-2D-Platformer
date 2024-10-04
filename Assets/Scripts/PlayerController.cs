a977b7b54e475d22386f3df0c5ab562e83853e6b
WillShaw1100 <williamrockstar110@gmail.com>
2024-10-04 15:16:07 +0100

    Add Jump, Platforms and Camera Effects

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody2D rigidbody;
-    public float speed = 5.0f;
-    public float jumpForce = 8.0f;
-    public float airControlForce = 10.0f;
-    public float airControlMax = 1.5f;
+    private Rigidbody2D rigidBody;
+    public float speed = 10.0f;
+    public float jumpForce = 85.0f;
+    public float airControlForce = 20.0f;
+    public float airControlMax = 2.5f;
+
+    Vector2 boxExtents;
     // Start is called before the first frame update
     void Start()
     {
-       rigidbody = GetComponent<Rigidbody2D>(); 
+        rigidBody = GetComponent<Rigidbody2D>();
+
+        // get the extent of the collision box
+        boxExtents = GetComponent<BoxCollider2D>().bounds.extents;
+
     }
 
     // Update is called once per frame
@@ -25,10 +31,29 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         float h = Input.GetAxis("Horizontal");
-        if (h != 0)
+        // check if we are on the ground
+        Vector2 bottom =
+        new Vector2(transform.position.x, transform.position.y - boxExtents.y);
+
+        Vector2 hitBoxSize = new Vector2(boxExtents.x * 2.0f, 0.05f);
+
+        RaycastHit2D result = Physics2D.BoxCast(bottom, hitBoxSize, 0.0f,
+        new Vector3(0.0f, -1.0f), 0.0f, 1 << LayerMask.NameToLayer("Ground"));
+
+        bool grounded = result.collider != null && result.normal.y > 0.9f;
+        if (grounded)
         {
-            
-            rigidbody.velocity = new Vector2(h * speed, 0);
+            if (Input.GetAxis("Jump") > 0.0f)
+                rigidBody.AddForce(new Vector2(0.0f, jumpForce), ForceMode2D.Impulse);
+            else
+                rigidBody.velocity = new Vector2(speed * h, rigidBody.velocity.y);
+        }
+        else
+        {
+            // allow a small amount of movement in the air
+            float vx = rigidBody.velocity.x;
+            if (h * vx < airControlMax)
+                rigidBody.AddForce(new Vector2(h * airControlForce, 0));
         }
     }
 }
